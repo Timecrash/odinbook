d5a9586618e5d5dfcddb7166807072de5ba7a444
Timecrash <mzvelez75@gmail.com>
2016-04-28 14:48:19 +0000

    Add friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,26 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   before_save :downcase_email
   
+  has_many :active_friendships, class_name:  "Friendship",
+                                foreign_key: "friender_id",
+                                dependent: :destroy
+  has_many :passive_friendships, class_name:  "Friendship",
+                                 foreign_key: "friended_id",
+                                 dependent: :destroy
+  has_many :active_friends,  through: :active_friendships,  source: :friender
+  has_many :passive_friends, through: :passive_friendships, source: :friended
+  
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
   format: { with: VALID_EMAIL_REGEX },
   uniqueness: { case_sensitive: false }
+  
+  def friends
+    friend_list = active_friends
+    friend_list << passive_friends
+    friend_list.uniq!
+  end
   
   private
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426144404) do
+ActiveRecord::Schema.define(version: 20160428135416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "friender_id"
+    t.integer  "friended_id"
+    t.boolean  "accepted"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
