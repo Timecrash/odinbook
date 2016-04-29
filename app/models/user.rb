a36be158b21daec3f241907017e9522bd2529fd5
Timecrash <mzvelez75@gmail.com>
2016-04-29 18:34:51 +0000

    Refactor User to remove extraneous validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   has_many :passive_friends, through: :passive_friendships, source: :friender
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   
