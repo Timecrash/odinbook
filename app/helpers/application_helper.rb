6bea52cd16cad258d251dd06508f26b8dff16466
Timecrash <mzvelez75@gmail.com>
2016-04-29 19:46:17 +0000

    Add full_title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  #Returns the full title on a per-page basis.
+  def full_title(page_title = "")
+    base_title = "Odinbook"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
