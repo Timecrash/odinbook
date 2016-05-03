c6282c02ae1132c08e2f1f2c872116c00305a5ca
Timecrash <mzvelez75@gmail.com>
2016-05-03 17:40:32 +0000

    Set routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     end
   end
   resources :friendships, only: [:create, :update]
+  resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
