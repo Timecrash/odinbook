8234d6d53b587db088a6ba7f185be5123f0cf261
Timecrash <mzvelez75@gmail.com>
2016-05-03 18:35:21 +0000

    Fill seeds.rb with fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(first_name:            "Matt",
+             last_name:             "Velez",
+             email:                 "matt@mattvelez.net",
+             password:              ENV["matt_password"],
+             password_confirmation: ENV["matt_password"])
+
+99.times do |n|
+  first_name = Faker::Name.first_name
+  last_name  = Faker::name.last_name
+  email      = first_name + last_name[0] + "@example.com"
+  password   = "password"
+  User.create!(first_name: first_name,
+               last_name:  last_name,
+               email:      email,
+               password:              password,
+               password_confirmation: password)
+end
