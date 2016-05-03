ed1d0be53ad10830ba76ac4e4185745a84486ad9
Timecrash <mzvelez75@gmail.com>
2016-05-03 18:52:35 +0000

    Update User Index to display users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ User.create!(first_name:            "Matt",
 
 99.times do |n|
   first_name = Faker::Name.first_name
-  last_name  = Faker::name.last_name
-  email      = first_name + last_name[0] + "@example.com"
+  last_name  = Faker::Name.last_name
+  email      = Faker::Internet.safe_email(first_name)
   password   = "password"
   User.create!(first_name: first_name,
                last_name:  last_name,
