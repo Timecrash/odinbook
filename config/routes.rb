c2209e51933419d77f0997e1d1d1fe3020f9ad87
Timecrash <mzvelez75@gmail.com>
2016-05-03 20:00:28 +0000

    Displays posts on user show

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     end
   end
   resources :friendships, only: [:create, :update, :destroy]
-  resources :posts
+  resources :posts, except: :new
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ User.create!(first_name:            "Matt",
 99.times do |n|
   first_name = Faker::Name.first_name
   last_name  = Faker::Name.last_name
-  email      = Faker::Internet.safe_email(first_name)
+  email      = Faker::Internet.safe_email(first_name + n)
   password   = "password"
-  User.create!(first_name: first_name,
-               last_name:  last_name,
-               email:      email,
+  User.create!(first_name:            first_name,
+               last_name:             last_name,
+               email:                 email,
                password:              password,
                password_confirmation: password)
 end
