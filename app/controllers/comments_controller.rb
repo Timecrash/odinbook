20a0321384609b28bb08a8528e82197070efaa2e
Timecrash <mzvelez75@gmail.com>
2016-05-08 17:47:34 +0000

    Prepare for adding Comment form to views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    comment = current_user.comments.build(post_id: params[:post_id], comment_params)
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:text, :post_id)
+  end
 end
