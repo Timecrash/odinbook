2f2f4ad8c5c8ee65dc5c9463eb5b35c833ccb5c1
Timecrash <mzvelez75@gmail.com>
2016-05-08 17:24:48 +0000

    Set up Comment tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "comments must be attached to a user" do
+    comment = posts(:remilia).comments.build()
+    assert_not comment.valid?
+  end
+  
+  test "comments must be attached to a post" do
+    comment = users(:reimu).comments.build()
+    assert_not comment.valid?
+  end
+  
+  test "comments must have text" do
+    comment = users(:reimu).comments.build(post_id: posts(:remilia).id)
+    assert_not comment.valid?
+  end
 end
