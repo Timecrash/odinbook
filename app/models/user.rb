5fc72c7f33c369e85cb18a3e650baa1ac4e5ea22
Timecrash <mzvelez75@gmail.com>
2016-05-08 21:09:51 +0000

    Add OAuth Callback controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,24 @@ class User < ActiveRecord::Base
 
   #Add avatar code here, via paperclip.
   
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      #user.image = auth.info.image
+    end
+  end
+  
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+  
   def name
     "#{first_name} #{last_name}"
   end
