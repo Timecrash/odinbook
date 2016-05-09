b61dee85d47a05d150cac7f5f409f11dbe74d8d2
Timecrash <mzvelez75@gmail.com>
2016-05-09 19:38:04 +0000

    Added Confirmable module to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable and :timeoutable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
   before_save :downcase_email
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,23 @@
-User.create!(first_name:            "Matt",
-             last_name:             "Velez",
-             email:                 "matt@mattvelez.net",
-             password:              ENV["matt_password"],
-             password_confirmation: ENV["matt_password"])
+matt = User.create!(first_name:            "Matt",
+                    last_name:             "Velez",
+                    email:                 "matt@mattvelez.net",
+                    password:              ENV["matt_password"],
+                    password_confirmation: ENV["matt_password"])
+matt.confirm!
 
 99.times do |n|
   first_name = Faker::Name.first_name
   last_name  = Faker::Name.last_name
   email      = Faker::Internet.safe_email("#{first_name}-#{n+1}")
   password   = "password"
-  User.create!(first_name:            first_name,
-               last_name:             last_name,
-               email:                 email,
-               password:              password,
-               password_confirmation: password)
+  user = User.create!(first_name:            first_name,
+                      last_name:             last_name,
+                      email:                 email,
+                      password:              password,
+                      password_confirmation: password)
+  user.confirm!
 end
 
-matt = User.first
 User.second.friend(matt)
 matt.accept_friendship(User.second)
 User.last.friend(matt)
