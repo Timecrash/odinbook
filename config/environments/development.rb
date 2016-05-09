33783425ca4209f8625b6f2d8cfa1767817be3d8
Timecrash <mzvelez75@gmail.com>
2016-05-09 15:46:13 +0000

    Configure Devise's mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: '0.0.0.0:8080' }
+  config.action_mailer.delivery_method = :test
+  host = 'rails-projects-timecrash1.c9users.io'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
