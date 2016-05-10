ef164d4bc848b2e78eead95c8d9570195dc02ef7
Timecrash <mzvelez75@gmail.com>
2016-05-10 19:48:10 +0000

    Add picture to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :likes,    dependent: :destroy
   has_many :comments, dependent: :destroy
+  
+  has_attached_file :picture, styles: { medium: "400x400>" }
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+  
   default_scope -> { order(created_at: :desc) }
   validates :text, presence: true
   validates :user_id, presence: true
