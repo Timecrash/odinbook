58ee52c277dd39a4de0aa3f1e46d8238d881b601
Timecrash <mzvelez75@gmail.com>
2016-05-11 16:57:53 +0000

    Finalize post images

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
   has_many :likes,    dependent: :destroy
   has_many :comments, dependent: :destroy
   
-  has_attached_file :picture, styles: { medium: "400x400>" }
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { medium: "400x400>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   
   default_scope -> { order(created_at: :desc) }
   validates :text, presence: true
